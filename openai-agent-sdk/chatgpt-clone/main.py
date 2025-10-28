a08144b59a57a607cd299658feeb3eeeeb8d3c47
shinjunghun <61132132+ophnsjh0@users.noreply.github.com>
2025-10-28 16:51:49 +0900

    8. openAI-clone UI

--- openai-agent-sdk/chatgpt-clone/main.py
+++ openai-agent-sdk/chatgpt-clone/main.py
@@ -1,47 +1,69 @@
+import dotenv
+
+dotenv.load_dotenv()
+import asyncio
 import streamlit as st
-import time
+from agents import Agent, Runner, SQLiteSession
+
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="ChatGPT Clone",
+        instructions="""
+        You are a helpful assistant.
+        """,
+    )
+agent = st.session_state["agent"]
+
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history",
+        "chat-gpt-clone-memory.db",
+    )
+session = st.session_state["session"]
 
 
-st.header("Hello world!")
+async def paint_history():
+    messages = await session.get_items()
 
-st.button("Click me please!")
+    for message in messages:
+        with st.chat_message(message["role"]):
+            if message["role"] == "user":
+                st.write(message["content"])
+            else:
+                if message["type"] == "message":
+                    st.write(message["content"][0]["text"])
 
-st.text_input(
-    "Write your API KEY",
-    max_chars=20,
-)
 
-st.feedback("faces")
+asyncio.run(paint_history())
+
+
+async def run_agent(message):
+    with st.chat_message("ai"):
+        text_placeholder = st.empty()
+        response = ""
+        stream = Runner.run_streamed(
+            agent,
+            message,
+            session=session,
+        )
+
+        async for event in stream.stream_events():
+            if event.type == "raw_response_event":
+                if event.data.type == "response.output_text.delta":
+                    response += event.data.delta
+                    text_placeholder.write(response)
+
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
+    asyncio.run(run_agent(prompt))
+
 
 with st.sidebar:
-    st.badge("Badge 1")
-
-
-tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Outpu"])
-
-with tab1:
-    st.header("Agent")
-with tab2:
-    st.header("Agent 2")
-with tab3:
-    st.header("Agent 3")
-
-
-with st.chat_message("ai"):
-    st.text("Hello!")
-    with st.status("Agent is using tool") as status:
-        time.sleep(1)
-        status.update(label="Agent is searching the web....")
-        time.sleep(2)
-        status.update(label="Agent is reading the page....")
-        time.sleep(3)
-        status.update(state="complete")
-
-with st.chat_message("human"):
-    st.text("Hi!")
-
-
-st.chat_input(
-    "Write a message for the assistant.",
-    accept_file=True,
-)
+    reset = st.button("Reset memory")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
